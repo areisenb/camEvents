1483b921d7e3f67b6f6cc5f9a8e98218e661aea0
reisenba <reisenba@gmx.at>
2023-01-22 16:47:01 +0100

    make compatible with old nodejs versions as well

--- utils/event.js
+++ utils/event.js
@@ -3,7 +3,7 @@
 
 exports.composeEvtName = function(event) {
   let date = new Date;
-  return event + '@' + date.toISOString().replace (/([0-9]{2})\.[0-9]{3}Z/, "$1Z").replaceAll(':', '_');
+  return event + '@' + date.toISOString().replace (/([0-9]{2})\.[0-9]{3}Z/, "$1Z").replace(/:/g, '_');
 }
 
 exports.analyseEvent = function(eventName) {
@@ -11,6 +11,6 @@ exports.analyseEvent = function(eventName) {
   let name = split[0];
   let date = split[1];
   if (date && date.length >= 0) {
-    return ({ id: eventName.name, name: name, date: date.replaceAll('_', ':')});
+    return ({ id: eventName.name, name: name, date: date.replace(/_/g, ':')});
   } else return undefined;
 }
